cd1864fce3977323c995c400f41cbcc2da46f73a
Thor Brigsted <thorbrigsted@gmail.com>
2017-10-08 23:44:08 +0200

    Better asset handling on add/remove node

--- Scripts/NodeGraph.cs
+++ Scripts/NodeGraph.cs
@@ -18,7 +18,10 @@ public abstract class NodeGraph : ScriptableObject, ISerializationCallbackReceiv
     public virtual Node AddNode(Type type) {
         Node node = ScriptableObject.CreateInstance(type) as Node;
 #if UNITY_EDITOR
-        UnityEditor.AssetDatabase.AddObjectToAsset(node, this);
+        if (!Application.isPlaying) {
+            UnityEditor.AssetDatabase.AddObjectToAsset(node, this);
+            UnityEditor.AssetDatabase.SaveAssets();
+        }
 #endif
         nodes.Add(node);
         node.graph = this;
@@ -29,6 +32,12 @@ public abstract class NodeGraph : ScriptableObject, ISerializationCallbackReceiv
     /// <param name="node"></param>
     public void RemoveNode(Node node) {
         node.ClearConnections();
+#if UNITY_EDITOR
+        if (!Application.isPlaying) {
+            DestroyImmediate(node, true);
+            UnityEditor.AssetDatabase.SaveAssets();
+        }
+#endif
         nodes.Remove(node);
     }
 

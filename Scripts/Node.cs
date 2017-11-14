d68d926a2d5e3e4297fd6fb5d5b531aadc1a26d6
Thor Kramer Brigsted <thor1996@VID.net.local>
2017-11-14 16:50:28 +0100

    Added color32 support for NodeTint

--- Scripts/Node.cs
+++ Scripts/Node.cs
@@ -178,9 +178,9 @@ namespace XNode {
         public class NodeTint : Attribute {
             public Color color;
             /// <summary> Specify a color for this node type </summary>
-            /// <param name="r"> Red [0..1] </param>
-            /// <param name="g"> Green [0..1] </param>
-            /// <param name="b"> Blue [0..1] </param>
+            /// <param name="r"> Red [0.0f .. 1.0f] </param>
+            /// <param name="g"> Green [0.0f .. 1.0f] </param>
+            /// <param name="b"> Blue [0.0f .. 1.0f] </param>
             public NodeTint(float r, float g, float b) {
                 color = new Color(r, g, b);
             }
@@ -189,6 +189,14 @@ namespace XNode {
             /// <param name="hex"> HEX color value </param>
             public NodeTint(string hex) {
                 ColorUtility.TryParseHtmlString(hex, out color);
+            }
+
+            /// <summary> Specify a color for this node type </summary>
+            /// <param name="r"> Red [0 .. 255] </param>
+            /// <param name="g"> Green [0 .. 255] </param>
+            /// <param name="b"> Blue [0 .. 255] </param>
+            public NodeTint(byte r, byte g, byte b) {
+                color = new Color32(r, g, b, byte.MaxValue);
             }
         }
 

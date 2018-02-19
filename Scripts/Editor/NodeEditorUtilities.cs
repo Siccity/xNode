5ef958f298f42762c929d2f377d3282d156c19e6
Thor Brigsted <thor1996@VID.net.local>
2018-02-19 17:09:59 +0100

    Added c# script templates Create > xNode > Node/NodeGraph

--- Scripts/Editor/NodeEditorUtilities.cs
+++ Scripts/Editor/NodeEditorUtilities.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace XNodeEditor {
     /// <summary> A set of editor-only utilities and extensions for UnityNodeEditorBase </summary>
     public static class NodeEditorUtilities {
+
+        /// <summary>C#'s Script Icon [The one MonoBhevaiour Scripts have].</summary>
+        private static Texture2D scriptIcon = (EditorGUIUtility.IconContent("cs Script Icon").image as Texture2D);
 
         public static bool GetAttrib<T>(Type classType, out T attribOut) where T : Attribute {
             object[] attribs = classType.GetCustomAttributes(typeof(T), false);
@@ -84,6 +92,87 @@ namespace XNodeEditor {
                     return s.Substring(0, i) + "[" + rank + "]" + s.Substring(i);
                 }
             } else return type.ToString();
+        }
+
+        /// <summary>Creates a new C# Class.</summary>
+        [MenuItem("Assets/Create/xNode/Node C# Script", false, 89)]
+        private static void CreateNode() {
+            string[] guids = AssetDatabase.FindAssets("xNode_NodeTemplate.cs");
+            if (guids.Length == 0) {
+                Debug.LogWarning("xNode_NodeTemplate.cs.txt not found in asset database");
+                return;
+            }
+            string path = AssetDatabase.GUIDToAssetPath(guids[0]);
+            CreateFromTemplate(
+                "NewNode.cs",
+                path
+            );
+        }
+
+        /// <summary>Creates a new C# Class.</summary>
+        [MenuItem("Assets/Create/xNode/NodeGraph C# Script", false, 89)]
+        private static void CreateGraph() {
+            string[] guids = AssetDatabase.FindAssets("xNode_NodeGraphTemplate.cs");
+            if (guids.Length == 0) {
+                Debug.LogWarning("xNode_NodeGraphTemplate.cs.txt not found in asset database");
+                return;
+            }
+            string path = AssetDatabase.GUIDToAssetPath(guids[0]);
+            CreateFromTemplate(
+                "NewNodeGraph.cs",
+                path
+            );
+        }
+
+        public static void CreateFromTemplate(string initialName, string templatePath) {
+            ProjectWindowUtil.StartNameEditingIfProjectWindowExists(
+                0,
+                ScriptableObject.CreateInstance<DoCreateCodeFile>(),
+                initialName,
+                scriptIcon,
+                templatePath
+            );
+        }
+
+        /// Inherits from EndNameAction, must override EndNameAction.Action
+        public class DoCreateCodeFile : UnityEditor.ProjectWindowCallback.EndNameEditAction {
+            public override void Action(int instanceId, string pathName, string resourceFile) {
+                Object o = CreateScript(pathName, resourceFile);
+                ProjectWindowUtil.ShowCreatedAsset(o);
+            }
+        }
+
+        /// <summary>Creates Script from Template's path.</summary>
+        internal static UnityEngine.Object CreateScript(string pathName, string templatePath) {
+            string className = Path.GetFileNameWithoutExtension(pathName).Replace(" ", string.Empty);
+            string templateText = string.Empty;
+
+            UTF8Encoding encoding = new UTF8Encoding(true, false);
+
+            if (File.Exists(templatePath)) {
+                /// Read procedures.
+                StreamReader reader = new StreamReader(templatePath);
+                templateText = reader.ReadToEnd();
+                reader.Close();
+
+                templateText = templateText.Replace("#SCRIPTNAME#", className);
+                templateText = templateText.Replace("#NOTRIM#", string.Empty);
+                /// You can replace as many tags you make on your templates, just repeat Replace function
+                /// e.g.:
+                /// templateText = templateText.Replace("#NEWTAG#", "MyText");
+
+                /// Write procedures.
+
+                StreamWriter writer = new StreamWriter(Path.GetFullPath(pathName), false, encoding);
+                writer.Write(templateText);
+                writer.Close();
+
+                AssetDatabase.ImportAsset(pathName);
+                return AssetDatabase.LoadAssetAtPath(pathName, typeof(Object));
+            } else {
+                Debug.LogError(string.Format("The template file was not found: {0}", templatePath));
+                return null;
+            }
         }
     }
 }

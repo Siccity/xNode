794d1aefc7046ee55a690a6c7dd0902fc0ec40c7
Thor Brigsted <thorbrigsted@gmail.com>
2018-06-27 01:01:07 +0200

    Begun reworking editor caching

--- Scripts/Editor/NodeEditorBase.cs
+++ Scripts/Editor/NodeEditorBase.cs
@@ -2,36 +2,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
-using UnityEngine;
 using UnityEditor;
+using UnityEngine;
 
 namespace XNodeEditor.Internal {
 	/// <summary> Handles caching of custom editor classes and their target types. Accessible with GetEditor(Type type) </summary>
-	public class NodeEditorBase<T, A, K> where A : Attribute, NodeEditorBase<T, A, K>.INodeEditorAttrib where T : NodeEditorBase<T,A,K> where K : ScriptableObject {
+	public class NodeEditorBase<T, A, K> where A : Attribute, NodeEditorBase<T, A, K>.INodeEditorAttrib where T : NodeEditorBase<T, A, K> where K : ScriptableObject {
 		/// <summary> Custom editors defined with [CustomNodeEditor] </summary>
-		private static Dictionary<Type, T> editors;
+		private static Dictionary<Type, T> editorTemplates;
+		private static Dictionary<K, T> editors = new Dictionary<K, T>();
 		public K target;
 		public SerializedObject serializedObject;
 
 		public static T GetEditor(K target) {
 			if (target == null) return null;
-			Type type = target.GetType();
-			T editor = GetEditor(type);
-			editor.target = target;
-			editor.serializedObject = new SerializedObject(target);
-			return editor;
+			if (!editors.ContainsKey(target)) {
+				Type type = target.GetType();
+				T editor = GetEditor(type);
+				editors.Add(target, Activator.CreateInstance(editor.GetType()) as T);
+				editors[target].target = target;
+				editors[target].serializedObject = new SerializedObject(target);
+				return editor;
+			}
+			return editors[target];
+
 		}
 
 		private static T GetEditor(Type type) {
 			if (type == null) return null;
-			if (editors == null) CacheCustomEditors();
-			if (editors.ContainsKey(type)) return editors[type];
+			if (editorTemplates == null) CacheCustomEditors();
+			if (editorTemplates.ContainsKey(type)) return editorTemplates[type];
 			//If type isn't found, try base type
 			return GetEditor(type.BaseType);
 		}
 
 		private static void CacheCustomEditors() {
-			editors = new Dictionary<Type, T>();
+			editorTemplates = new Dictionary<Type, T>();
 
 			//Get all classes deriving from NodeEditor via reflection
 			Type[] nodeEditors = XNodeEditor.NodeEditorWindow.GetDerivedTypes(typeof(T));
@@ -40,7 +46,7 @@ namespace XNodeEditor.Internal {
 				if (attribs == null || attribs.Length == 0) continue;
 				if (nodeEditors[i].IsAbstract) continue;
 				A attrib = attribs[0] as A;
-				editors.Add(attrib.GetInspectedType(), Activator.CreateInstance(nodeEditors[i]) as T);
+				editorTemplates.Add(attrib.GetInspectedType(), Activator.CreateInstance(nodeEditors[i]) as T);
 			}
 		}
 

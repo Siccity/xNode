d68aea2a8a59c023eff467f36676e72712cc6222
Thor Brigsted <thorbrigsted@gmail.com>
2019-05-10 09:55:10 +0200

    Fixed #142 - New feature: Automatically remove "Node" postfix in names

--- Scripts/Editor/NodeGraphEditor.cs
+++ Scripts/Editor/NodeGraphEditor.cs
@@ -72,7 +72,12 @@ namespace XNodeEditor {
         public virtual void CreateNode(Type type, Vector2 position) {
             XNode.Node node = target.AddNode(type);
             node.position = position;
-            if (string.IsNullOrEmpty(node.name)) node.name = UnityEditor.ObjectNames.NicifyVariableName(type.Name);
+            if (string.IsNullOrEmpty(node.name)) {
+                // Automatically remove redundant 'Node' postfix
+                string typeName = type.Name;
+                if (typeName.EndsWith("Node")) typeName = typeName.Substring(0, typeName.LastIndexOf("Node"));
+                node.name = UnityEditor.ObjectNames.NicifyVariableName(typeName);
+            }
             AssetDatabase.AddObjectToAsset(node, target);
             if (NodeEditorPreferences.GetSettings().autoSave) AssetDatabase.SaveAssets();
             NodeEditorWindow.RepaintAll();

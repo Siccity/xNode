e2067b684506d0e0c96f106ffabf6924c4580574
Thor Brigsted <thorbrigsted@gmail.com>
2019-06-12 09:33:26 +0200

    Fixed formatting

--- Scripts/NodeDataCache.cs
+++ Scripts/NodeDataCache.cs
@@ -58,8 +58,7 @@ namespace XNode {
                 // ignore all unity related assemblies
                 // never ignore current executing assembly
                 foreach (Assembly assembly in assemblies) {
-                    if(assembly != Assembly.GetExecutingAssembly())
-					{
+                    if(assembly != Assembly.GetExecutingAssembly()) {
                         if (assembly.FullName.StartsWith("Unity")) continue;
                         // unity created assemblies always have version 0.0.0
                         if (!assembly.FullName.Contains("Version=0.0.0")) continue;

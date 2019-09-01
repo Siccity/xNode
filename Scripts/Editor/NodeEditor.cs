43bcb54fda4378e46a678c866242954248a63702
Thor Brigsted <thorbrigsted@gmail.com>
2019-09-01 23:49:19 +0200

    Odin inspector support (#182)
    * Added Odin Inspector support
    * Added support for ports

--- Scripts/Editor/NodeEditor.cs
+++ Scripts/Editor/NodeEditor.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
+#if ODIN_INSPECTOR
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities;
+using Sirenix.Utilities.Editor;
+#endif
 
 namespace XNodeEditor {
     /// <summary> Base class to derive custom Node editors from. Use this to create your own custom inspectors and editors for your nodes. </summary>
@@ -10,10 +15,14 @@ namespace XNodeEditor {
     public class NodeEditor : XNodeEditor.Internal.NodeEditorBase<NodeEditor, NodeEditor.CustomNodeEditorAttribute, XNode.Node> {
 
         private readonly Color DEFAULTCOLOR = new Color32(90, 97, 105, 255);
-        
+
         /// <summary> Fires every whenever a node was modified through the editor </summary>
         public static Action<XNode.Node> onUpdateNode;
         public readonly static Dictionary<XNode.NodePort, Vector2> portPositions = new Dictionary<XNode.NodePort, Vector2>();
+
+#if ODIN_INSPECTOR
+        internal static bool inNodeEditor = false;
+#endif
 
         public virtual void OnHeaderGUI() {
             GUILayout.Label(target.name, NodeEditorResources.styles.nodeHeader, GUILayout.Height(30));
@@ -21,11 +30,23 @@ namespace XNodeEditor {
 
         /// <summary> Draws standard field editors for all public fields </summary>
         public virtual void OnBodyGUI() {
+#if ODIN_INSPECTOR
+            inNodeEditor = true;
+#endif
+
             // Unity specifically requires this to save/update any serial object.
             // serializedObject.Update(); must go at the start of an inspector gui, and
             // serializedObject.ApplyModifiedProperties(); goes at the end.
             serializedObject.Update();
             string[] excludes = { "m_Script", "graph", "position", "ports" };
+
+#if ODIN_INSPECTOR
+            InspectorUtilities.BeginDrawPropertyTree(objectTree, true);
+            GUIHelper.PushLabelWidth(84);
+            objectTree.Draw(true);
+            InspectorUtilities.EndDrawPropertyTree(objectTree);
+            GUIHelper.PopLabelWidth();
+#else
 
             // Iterate through serialized properties and draw them like the Inspector (But with ports)
             SerializedProperty iterator = serializedObject.GetIterator();
@@ -36,6 +57,7 @@ namespace XNodeEditor {
                 if (excludes.Contains(iterator.name)) continue;
                 NodeEditorGUILayout.PropertyField(iterator, true);
             }
+#endif
 
             // Iterate through dynamic ports and draw them in the order in which they are serialized
             foreach (XNode.NodePort dynamicPort in target.DynamicPorts) {
@@ -44,6 +66,20 @@ namespace XNodeEditor {
             }
 
             serializedObject.ApplyModifiedProperties();
+
+#if ODIN_INSPECTOR
+            // Call repaint so that the graph window elements respond properly to layout changes coming from Odin    
+            if (GUIHelper.RepaintRequested) {
+                GUIHelper.ClearRepaintRequest();
+                window.Repaint();
+            }
+#else
+            window.Repaint();
+#endif
+
+#if ODIN_INSPECTOR
+            inNodeEditor = false;
+#endif
         }
 
         public virtual int GetWidth() {

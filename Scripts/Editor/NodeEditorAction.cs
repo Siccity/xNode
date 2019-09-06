df002dbe978f793d177f60f332f9ff09a39a2653
Thor Brigsted <thorbrigsted@gmail.com>
2019-09-06 18:55:09 +0200

    Removed duplicate RerouteReference

--- Scripts/Editor/NodeEditorAction.cs
+++ Scripts/Editor/NodeEditorAction.cs
@@ -31,24 +31,6 @@ namespace XNodeEditor {
         private RerouteReference[] preBoxSelectionReroute;
         private Rect selectionBox;
         private bool isDoubleClick = false;
-
-        private struct RerouteReference {
-            public XNode.NodePort port;
-            public int connectionIndex;
-            public int pointIndex;
-
-            public RerouteReference(XNode.NodePort port, int connectionIndex, int pointIndex) {
-                this.port = port;
-                this.connectionIndex = connectionIndex;
-                this.pointIndex = pointIndex;
-            }
-
-            public void InsertPoint(Vector2 pos) { port.GetReroutePoints(connectionIndex).Insert(pointIndex, pos); }
-            public void SetPoint(Vector2 pos) { port.GetReroutePoints(connectionIndex) [pointIndex] = pos; }
-            public void RemovePoint() { port.GetReroutePoints(connectionIndex).RemoveAt(pointIndex); }
-            public Vector2 GetPoint() { return port.GetReroutePoints(connectionIndex) [pointIndex]; }
-        }
-
         private Vector2 lastMousePosition;
 
         public void Controls() {

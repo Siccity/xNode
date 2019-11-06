36316e83b898826b62ede0f4c3af996440570e9f
Thor Brigsted <thorbrigsted@gmail.com>
2019-11-06 11:34:09 +0100

    Flipped output inherited type constraints, edited comments

--- Scripts/Node.cs
+++ Scripts/Node.cs
@@ -45,11 +45,11 @@ namespace XNode {
         public enum TypeConstraint {
             /// <summary> Allow all types of input</summary>
             None,
-            /// <summary> Allow similar and inherited types </summary>
+            /// <summary> Allow connections where input value type is assignable from output value type (eg. ScriptableObject --> Object)</summary>
             Inherited,
             /// <summary> Allow only similar types </summary>
             Strict,
-            /// <summary> Allow similar and inherited types but checks inheritance in reverse </summary>
+            /// <summary> Allow connections where output value type is assignable from input value type (eg. Object --> ScriptableObject)</summary>
             InheritedInverse,
         }
 

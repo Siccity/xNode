7e93ffe4b75a6cab453223d20c5e03f4d7c57f7f
Lumos <magesnika@gmail.com>
2020-01-21 07:01:57 +0000

    Fix for the dynamic port "serialisation desync" problem. (#223)
    * Exclude strings from array types manually to prevent them from being treated as char arrays.

--- Scripts/Node.cs
+++ Scripts/Node.cs
@@ -119,12 +119,12 @@ namespace XNode {
         protected void OnEnable() {
             if (graphHotfix != null) graph = graphHotfix;
             graphHotfix = null;
-            UpdateStaticPorts();
+            UpdatePorts();
             Init();
         }
 
-        /// <summary> Update static ports to reflect class fields. This happens automatically on enable. </summary>
-        public void UpdateStaticPorts() {
+        /// <summary> Update static ports and dynamic ports managed by DynamicPortLists to reflect class fields. This happens automatically on enable or on redrawing a dynamic port list. </summary>
+        public void UpdatePorts() {
             NodeDataCache.UpdatePorts(this, ports);
         }
 

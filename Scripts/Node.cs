a61bb3f87ea87a4851e4524b9c5f72f8835ae28f
Simon Rodriguez <mrsimonrodriguez@gmail.com>
2020-04-14 08:11:21 +0200

    Implemented OrderBy in Add Node Menu (#247)

--- Scripts/Node.cs
+++ Scripts/Node.cs
@@ -317,10 +317,20 @@ namespace XNode {
         [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
         public class CreateNodeMenuAttribute : Attribute {
             public string menuName;
+            public int order;
             /// <summary> Manually supply node class with a context menu path </summary>
             /// <param name="menuName"> Path to this node in the context menu. Null or empty hides it. </param>
             public CreateNodeMenuAttribute(string menuName) {
                 this.menuName = menuName;
+                this.order = 0;
+            }
+
+            /// <summary> Manually supply node class with a context menu path </summary>
+            /// <param name="menuName"> Path to this node in the context menu. Null or empty hides it. </param>
+            /// <param name="order"> The order by which the menu items are displayed. </param>
+            public CreateNodeMenuAttribute(string menuName, int order) {
+                this.menuName = menuName;
+                this.order = order;
             }
         }
 

da9af2b100d77d5bf3e5bc5f2b7f5bf96de7c1b8
MowfaqAlarbi <54871067+MowfaqAlarbi@users.noreply.github.com>
2021-07-10 09:26:36 +0200

    Delete Scripts directory

--- Scripts/Attributes/NodeEnum.cs
+++
@@ -1,5 +0,0 @@
-﻿using UnityEngine;
-
-/// <summary> Draw enums correctly within nodes. Without it, enums show up at the wrong positions. </summary>
-/// <remarks> Enums with this attribute are not detected by EditorGui.ChangeCheck due to waiting before executing </remarks>
-public class NodeEnumAttribute : PropertyAttribute { }
